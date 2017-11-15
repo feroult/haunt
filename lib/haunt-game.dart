3df6dc2d22001772015e12e39b1532ab5863328f
feroult <feroult@gmail.com>
2017-11-15 16:18:51 -0200

    parallax

--- lib/haunt-game.dart
+++ lib/haunt-game.dart
@@ -1,41 +1,72 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/component.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/painting.dart';
 
 class HauntGame extends Game {
   Size dimensions;
 
-  var layers = new List<Layer>();
-
-  var loaded = false;
+  ParallaxComponent background;
 
   HauntGame(this.dimensions) {
-    for (var i = 7; i >= 1; i--) {
-      layers.add(new Layer("layers/layer_0${i}_1920x1080.png", dimensions));
+    var filenames = new List<String>();
+    for (var i = 1; i <= 7; i++) {
+      filenames.add("layers/layer_0${i}.png");
     }
-    loaded = true;
+
+    background = new ParallaxComponent(dimensions, filenames);
   }
 
   @override
   void render(Canvas canvas) {
-    layers.forEach((layer) {
-      layer.render(canvas);
-    });
+    background.render(canvas);
   }
 
   @override
   void update(double t) {}
 }
 
-class Layer extends SpriteComponent {
-  Layer(String uri, Size dimensions)
-      : super.rectangle(dimensions.width, dimensions.height, uri) {
-    this.x = 0.0;
-    this.y = 0.0;
-    this.angle = 0.0;
+class ParallaxComponent extends PositionComponent {
+  List<Image> images = new List();
+  bool loaded = false;
+  Size dimensions;
+
+  ParallaxComponent(this.dimensions, List<String> filenames) {
+    _load(filenames);
+  }
+
+  void _load(List<String> filenames) {
+    var futures =
+        filenames.fold(new List<Future>(), (List<Future> result, filename) {
+      result.add(Flame.images.load(filename).then((image) {
+        images.add(image);
+      }));
+      return result;
+    });
+    Future.wait(futures).then((r) {
+      loaded = true;
+    });
   }
 
   @override
-  void update(double t) {}
+  void render(Canvas canvas) {
+    if (!loaded) {
+      return;
+    }
+    images.forEach((image) {
+      final Rect rect =
+          new Rect.fromLTWH(0.0, 0.0, dimensions.width, dimensions.height);
+      canvas.translate(x, y);
+      canvas.rotate(angle); // TODO: rotate around center
+      paintImage(canvas: canvas, image: image, rect: rect, fit: BoxFit.cover);
+    });
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+  }
 }
