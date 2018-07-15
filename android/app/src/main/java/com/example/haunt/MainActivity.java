7ee40f8415246720815f1f3c1e47c311f560d7db
feroult <feroult@gmail.com>
2018-07-15 13:59:44 -0300

    haunt

--- android/app/src/main/java/com/example/haunt/MainActivity.java
+++ android/app/src/main/java/com/example/haunt/MainActivity.java
@@ -1,7 +1,6 @@
-package com.yourcompany.haunt;
+package com.example.haunt;
 
 import android.os.Bundle;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
--- lib/ninja_world.dart
+++ lib/ninja_world.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 import 'dart:ui';
 
-import 'package:box2d/box2d.dart';
+import 'package:box2d_flame/box2d.dart';
 import 'package:flame/box2d/box2d_component.dart';
 import 'package:flame/box2d/viewport.dart';
 import 'package:flame/components/parallax_component.dart';
